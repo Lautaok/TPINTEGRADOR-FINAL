7e27f103d39551eea70581c843ea38f0f6c0554a
lautaro vera <lautarovera1709@gmail.com>
2024-08-12 14:49:09 -0300

    cambios

---
+++ src/Componentes/Tarjeta/Tarjeta.jsx
@@ -0,0 +1,10 @@
+import './Tarjeta.css'
+
+function Tarjetaquincho() {
+  return (
+    <div className='contenedor-tarjeta'>
+   <p className="parrafo-tarjeta">Sabemos que te apasiona jugar, pero una cosa lleva a la otra, y quien dice partidito también dice asado con amigos. Por eso, te resolvemos el problema. No hace falta que busques donde comer el asado post partido, ¡podés hacerlo acá!</p>
+  </div>);
+}
+
+export default Tarjetaquincho;
